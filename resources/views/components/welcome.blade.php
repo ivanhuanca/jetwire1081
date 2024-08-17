f8bad63aa16d3159bf476e92b22df25f5e4c3168
Ivan Huanca <81381865+ivanhuanca@users.noreply.github.com>
2024-08-17 18:42:08 -0500

    0.0.1

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -2,15 +2,9 @@
     <x-application-logo class="block h-12 w-auto" />
 
     <h1 class="mt-8 text-2xl font-medium text-gray-900 dark:text-white">
-        Welcome to your Jetstream application!
+        Bienvenidos a PUBLIHIT
     </h1>
 
-    <p class="mt-6 text-gray-500 dark:text-gray-400 leading-relaxed">
-        Laravel Jetstream provides a beautiful, robust starting point for your next Laravel application. Laravel is designed
-        to help you build your application using a development environment that is simple, powerful, and enjoyable. We believe
-        you should love expressing your creativity through programming, so we have spent time carefully crafting the Laravel
-        ecosystem to be a breath of fresh air. We hope you love it.
-    </p>
 </div>
 
 <div class="bg-gray-200 dark:bg-gray-800 bg-opacity-25 grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8 p-6 lg:p-8">
@@ -20,12 +14,12 @@
                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
             </svg>
             <h2 class="ms-3 text-xl font-semibold text-gray-900 dark:text-white">
-                <a href="https://laravel.com/docs">Documentation</a>
+                <a href="https://laravel.com/docs">Documentación</a>
             </h2>
         </div>
 
         <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-            Laravel has wonderful documentation covering every aspect of the framework. Whether you're new to the framework or have previous experience, we recommend reading all of the documentation from beginning to end.
+            Documentación de Laravel Jetstream, un marco de aplicación de inicio diseñado por el equipo de Laravel. Jetstream proporciona las características necesarias para construir aplicaciones modernas y robustas, como autenticación de dos factores, equipos, API de API y mucho más.
         </p>
 
         <p class="mt-4 text-sm">
@@ -50,7 +44,7 @@
         </div>
 
         <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-            Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript development. Check them out, see for yourself, and massively level up your development skills in the process.
+            Laracasts ofrece miles de videos de screencast de alta calidad sobre desarrollo web, que te permiten seguir a tu propio ritmo. Los temas incluyen Laravel, PHP, Vue, React, TDD, y más.
         </p>
 
         <p class="mt-4 text-sm">
@@ -75,7 +69,7 @@
         </div>
 
         <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-            Laravel Jetstream is built with Tailwind, an amazing utility first CSS framework that doesn't get in your way. You'll be amazed how easily you can build and maintain fresh, modern designs with this wonderful framework at your fingertips.
+            Laravel Jetstream esta construido con Tailwind CSS, un marco de diseño de componentes de utilidad de bajo nivel para construir rápidamente diseños personalizados. Tailwind CSS viene con una amplia gama de clases de utilidad y componentes preconstruidos para acelerar el desarrollo de su aplicación.
         </p>
     </div>
 
@@ -85,12 +79,12 @@
                 <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
             </svg>
             <h2 class="ms-3 text-xl font-semibold text-gray-900 dark:text-white">
-                Authentication
+                Authenticación
             </h2>
         </div>
 
         <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-            Authentication and registration views are included with Laravel Jetstream, as well as support for user email verification and resetting forgotten passwords. So, you're free to get started with what matters most: building your application.
+            Laravel Jetstream proporciona una autenticación de usuario robusta y segura. Las características incluyen autenticación de dos factores, verificación de correo electrónico, restablecimiento de contraseña, inicio de sesión de navegador y API de API.
         </p>
     </div>
 </div>
